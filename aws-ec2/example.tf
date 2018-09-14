1136def9b2a3bc1130de29b368612a163ca96e5b
novemberde@nate.com <novemberde@nate.com>
2018-09-14 16:31:31 +0900

    Add an ec2 example

--- aws-ec2/example.tf
+++ aws-ec2/example.tf
@@ -1,20 +1,4 @@
-variable "ami" {
-  description = "the AMI to use"  
-}
-
-/* A multi
-   line comment. */ 
-resource "aws_instance" "web" {
-  ami               = "${var.ami}" 
-  instance_type     = "t2.micro"
-  count             = 1
-  source_dest_check = false
-#   description = <<EOF
-#   ...
-#   ...
-#   EOF
-
-  connection {
-    user = "root"
-  }
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
 }
